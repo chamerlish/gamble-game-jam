1cd0cf19e8357ea447e9dce4d1203ce4a7041bf2
Chamerli <fares.setbel@proton.me>
2026-01-23 12:10:49 +0100

    basic tilemap with player movements
    tilemap from kenney store

---
+++ Scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const SPEED := 200.0
+const ACCEL := 1200.0
+
+@export var player_camera: Camera2D
+@export var camera_follow_speed := 5.0
+
+
+func _ready() -> void:
+	player_camera.global_position = global_position
+
+
+func _physics_process(delta: float) -> void:
+	var input_dir := Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+	
+	if input_dir != Vector2.ZERO:
+		input_dir = input_dir.normalized()
+		var target_velocity = input_dir * SPEED
+		velocity = velocity.move_toward(target_velocity, ACCEL * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, ACCEL * delta)
+
+	move_and_slide()
+	camera_follow_player(delta)
+
+
+func camera_follow_player(delta: float) -> void:
+	player_camera.global_position = player_camera.global_position.lerp(
+		global_position,
+		camera_follow_speed * delta
+	)
