df36e6daef43c1780c8ebc115756490363f6ac59
Chamerli <fares.setbel@proton.me>
2026-01-30 15:14:01 +0100

    added the counter

---
+++ Scripts/counter.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+var player_inside: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$InterractLabel.hide()
+	GlobalMachine.entity_list.append(self)
+	Global.expand.connect(expand)
+
+func expand() -> void:
+	position.x += Global.TILE_SIZE.y + Global.GRID_SIZE.y / 4
+	position.y += Global.TILE_SIZE.x + Global.GRID_SIZE.y / 4
+
+func _process(delta: float) -> void:
+	if position.y > Global.player_node.position.y + 30:
+		z_index = 100
+	else: z_index = -1
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		if player_inside:
+			Global.change_night_state()
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == Global.player_node:
+		$InterractLabel.hide(); 
+		player_inside = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == Global.player_node:
+		$InterractLabel.show(); 
+		player_inside = true
